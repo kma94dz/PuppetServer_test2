2ac2b8e314a7b84de17f952983ce214d7c188c05
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-06 15:07:20 -0400

    Update firstmanifest.pp

--- manifests/firstmanifest.pp
+++ manifests/firstmanifest.pp
@@ -2,3 +2,8 @@
   ensure => present,
   content => 'from git hub changed',
 }
+if $OperatingSystem != 'Linux' { 
+   warning('This manifest is not supported on this other OS apart from linux.') 
+} else { 
+   notify { 'the OS is Linux. We are good to go!': }
+} 
