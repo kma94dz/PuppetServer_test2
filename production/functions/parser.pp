f1e1a5b2c6ec419eb2a1cd3af01d538ff0966f6b
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 16:20:26 -0400

    Update parser.pp

--- production/functions/parser.pp
+++ production/functions/parser.pp
@@ -1,8 +1,15 @@
 # See https://puppet.com/docs/puppet/latest/lang_write_functions_in_puppet.html
 # for more information on native puppet functions.
-function parser(String $value) >> Integer {
+function parser(String $value) >> Array {
     $a = $value.split('/')
-    $n = $a.length()
+    $Directories = []
+    $i=0
+    $fullpath = ""
+    $a.each |$string|{
+        $fullpath = fullpath+$string
+        $Directories[$i] = $fullpath + "/"
+        $i=$i+1
+    }
     return $n
     
 }
