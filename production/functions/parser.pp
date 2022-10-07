05ee06a4f69191c090bbc6a3e10817a22fed3061
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 15:41:42 -0400

    Update parser.pp

--- production/functions/parser.pp
+++ production/functions/parser.pp
@@ -1,5 +1,5 @@
 # See https://puppet.com/docs/puppet/latest/lang_write_functions_in_puppet.html
 # for more information on native puppet functions.
 function parser(String $value) >> String {
-    $value
+    $value.split('/', -1)
 }
