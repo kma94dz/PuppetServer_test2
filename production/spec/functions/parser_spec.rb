3a2dd36678a284576c3b52a949e94024e910581a
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 21:52:53 -0400

    Update parser_spec.rb

--- production/spec/functions/parser_spec.rb
+++ production/spec/functions/parser_spec.rb
@@ -8,13 +8,22 @@ describe 'parser' do
   # with your expectations
   it { is_expected.to run.with_params(nil).and_raise_error(StandardError) }
 
-  Puppet::Functions.create_function(:'upcase') do
-    dispatch :up do
+  Puppet::Functions.create_function(:'get whats etween brackets') do
+    dispatch :extract_from_bracket do
       param 'String', :some_string
     end
   
-    def up(some_string)
-      return some_string.upcase
+    def extract_from_bracket(some_string)
+      for i in 0..some_string.length do
+        count=1
+        if some_string[i]=="("
+          for j in i..some_string.length do
+            if some_string[j]=="("
+              count = count+1
+            if some_string[j]==")"
+              count=count-1
+            if count == 0
+              break        
     end
   end
 end
