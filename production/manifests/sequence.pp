81739f0886790c8d873fc09d4732b434d6fddcf8
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 22:26:26 -0400

    Add files via upload

---
+++ production/manifests/sequence.pp
@@ -0,0 +1,23 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include train::sequence
+class production::sequence {
+########## on peut reecrir comme suit
+$directoryPath = ['/tmp/dir1/', '/tmp/dir1/dir2/', '/tmp/dir1/dir2/dir3/']
+
+file { '/tmp/dir1/dir2/dir3/part1.txt':
+  ensure => present,
+  before => File[['/tmp/dir1/dir2/dir3/part2.txt', '/tmp/dir1/dir2/dir3/part3.txt']],
+  require => File[$directoryPath],
+  content => extract_from_bracket('hello'),
+}
+file { ['/tmp/dir1/dir2/dir3/part2.txt', '/tmp/dir1/dir2/dir3/part3.txt']:
+  ensure => present,
+}
+file { $directoryPath:
+  ensure => 'directory',
+}
+}
