048162c1858b869467aa51ac1b61af4229063e4b
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 23:55:55 -0400

    Update path_to_array.rb

--- production/lib/puppet/functions/path_to_array.rb
+++ production/lib/puppet/functions/path_to_array.rb
@@ -6,16 +6,6 @@ Puppet::Functions.create_function(:'path_to_array') do
     def pta(path)
       paths=["n"] 
       dir="." 
-      for i in 0..path.length do
-        dir=dir + path[i]
-        if path[i]=="/" then
-          paths.push(dir)
-        end
-        paths.push(dir)
-      end
-      if path[path.legth] != "/" then
-        paths.push(dir)
-      end
       return paths       
     end
 end
