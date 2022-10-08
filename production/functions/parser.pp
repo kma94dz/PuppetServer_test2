7c0af61993f1399fb8197215aedce1da42bcb01b
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 22:22:19 -0400

    Delete production directory

--- production/functions/parser.pp
+++
@@ -1,5 +0,0 @@
-# See https://puppet.com/docs/puppet/latest/lang_write_functions_in_puppet.html
-# for more information on native puppet functions.
-function parser(String $value) >> String {
-    $value
-}
