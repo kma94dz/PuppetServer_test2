2b4cf70c683968ec751c01c683c48ee2a2c8d38d
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 22:50:40 -0400

    Add files via upload

---
+++ production/lib/puppet/functions/extract_from_brackets.rb
@@ -0,0 +1,29 @@
+Puppet::Functions.create_function(:'extract_from_brackets') do
+    dispatch :extract do
+      param 'String', :some_string
+    end
+  
+    def extract(some_string)
+      elems=[]  
+      for i in 0..some_string.length do
+        count=0
+        if some_string[i]=="(" then
+          for j in i..some_string.length do
+            if some_string[j]=="(" then
+              count = count+1
+            end  
+            if some_string[j]==")" then
+              count=count-1
+            end
+            if count == 0 then
+              for c in i..j do
+                elems.push(some_string[c])
+              end  
+              break
+            end
+          end
+        end
+      end
+      return elems        
+    end
+end
