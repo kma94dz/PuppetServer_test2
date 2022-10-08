d726e2f68ec2b4bf27fce239528cb7dbe9339f65
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-08 00:23:36 -0400

    Update sequence.pp

--- production/manifests/sequence.pp
+++ production/manifests/sequence.pp
@@ -6,7 +6,7 @@
 #   include train::sequence
 class production::sequence {
 ########## on peut reecrir comme suit
-$directoryPath = '/tmp/dir1/dir2/dir3/dir4/'
+$directoryPath = '/tmp/dir1/dir2/dir3/dir4/dir5'
 
 file { '/tmp/dir1/dir2/dir3/part1.txt':
   ensure => present,
