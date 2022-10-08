757e57a22ab293e8d4315d3bd33b2436647602b5
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 22:38:20 -0400

    Update extract_from_brackets.rb

--- production/lib/puppet/functions/extract_from_brackets.rb
+++ production/lib/puppet/functions/extract_from_brackets.rb
@@ -6,13 +6,19 @@ Puppet::Functions.create_function(:'extract_from_brackets') do
     def extract(some_string)
       for i in 0..some_string.length do
         count=1
-        if some_string[i]=="("
+        if some_string[i]=="(" then
           for j in i..some_string.length do
-            if some_string[j]=="("
+            if some_string[j]=="(" then
               count = count+1
-            if some_string[j]==")"
+            end  
+            if some_string[j]==")" then
               count=count-1
-            if count == 0
-              break        
+            end
+            if count == 0 then
+              break
+            end
+          end
+        end
+      end        
     end
 end
