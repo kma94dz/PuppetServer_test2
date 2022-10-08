a570789943a459bf0e9e2440c31fa0cb8c6d58c1
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-07 21:32:18 -0400

    Add files via upload

---
+++ production/spec/functions/parser_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'parser' do
+  # please note that these tests are examples only
+  # you will need to replace the params and return value
+  # with your expectations
+  it { is_expected.to run.with_params(nil).and_raise_error(StandardError) }
+
+  Puppet::Functions.create_function(:'upcase') do
+    dispatch :up do
+      param 'String', :some_string
+    end
+  
+    def up(some_string)
+      return some_string.upcase
+    end
+  end
+end
