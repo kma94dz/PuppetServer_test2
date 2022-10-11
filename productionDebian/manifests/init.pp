28c0dc59c8f10194beb8a44938130accfd105b9c
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 10:03:26 -0400

    Update init.pp

--- productionDebian/manifests/init.pp
+++ productionDebian/manifests/init.pp
@@ -4,5 +4,5 @@
 #include train::php_install
 file { '/tmp/fromGithub2.txt':
   ensure => present,
-  content => productionDebian::path_to_array('/tmp/aa/bb/cc/'),
+  content => environment::path_to_array('/tmp/aa/bb/cc/'),
 }
