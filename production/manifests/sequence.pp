218e916b5cc40b8441ebce38f8ffdd91bcfcd8a7
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 12:40:00 -0400

    Update sequence.pp

--- production/manifests/sequence.pp
+++ production/manifests/sequence.pp
@@ -10,12 +10,12 @@ $directoryPath = '/tmp/a/b/c/dir4/'
 
 file { '/tmp/a/b/c/part1.txt':
   ensure => present,
-  before => File[['/tmp/a/b/c/d/part2.txt', '/tmp/a/b/c/d/part3.txt']],
   require => File[$path_to_array($directoryPath)],
   content => String(path_to_array($directoryPath)),
 }
 file { ['/tmp/a/b/c/d/part2.txt', '/tmp/a/b/c/d/part3.txt']:
   ensure => present,
+  require => File['/tmp/a/b/c/part1.txt'],
 }
 file { $path_to_array($directoryPath)):
   ensure => 'directory',
