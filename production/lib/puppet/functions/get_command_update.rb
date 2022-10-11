250f775abcc8e7fad690125e8efdf187677a5ac9
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-10 23:14:34 -0400

    Update get_command_update.rb

--- production/lib/puppet/functions/get_command_update.rb
+++ production/lib/puppet/functions/get_command_update.rb
@@ -1,33 +1,27 @@
-require 'rbconfig'
-
-module OS
-  def self.name
-    case RbConfig::CONFIG['host_os']
-    
-    when /linux/
-      'Linux'
-    when /darwin/
-      'OS X'
-    when /mswin|mingw32|windows/
-      'Windows'
-    when /solaris/
-      'Solaris'
-    when /bsd/
-      'BSD'
-    else
-      RbConfig::CONFIG['host_os']
-    end
-  end
-end
-
-
-
 Puppet::Functions.create_function(:'get_command_update') do
   dispatch :cmd do
   
   end
 
   def cmd()
-    return OS.name       
+    r = { :distro => nil, :family => nil }
+
+    if File.exists?('/etc/lsb-release')
+      File.open('/etc/lsb-release', 'r').read.each_line do |line|
+        r = { :distro => $1 } if line =~ /^DISTRIB_ID=(.*)/
+      end
+    end
+  
+    if File.exists?('/etc/debian_version')
+      r[:distro] = 'Debian' if r[:distro].nil?
+      r[:family] = 'Debian' if r[:variant].nil?
+    elsif File.exists?('/etc/redhat-release') or File.exists?('/etc/centos-release')
+      r[:family] = 'RedHat' if r[:family].nil?
+      r[:distro] = 'CentOS' if File.exists?('/etc/centos-release')
+    elsif File.exists?('/etc/SuSE-release')
+      r[:distro] = 'SLES' if r[:distro].nil?
+    end
+  
+    return r   
   end
 end
