30f6f34b5e608a60b275b0e1f781418c451e07ca
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 15:00:47 -0400

    Update sequence.pp

--- production/manifests/sequence.pp
+++ production/manifests/sequence.pp
@@ -13,7 +13,7 @@ file { '/tmp/a/b/c/part1.txt':
   require => File[path_to_array($directoryPath)],
   content => String(path_to_array($directoryPath)),
 }
-$array = path_to_array($directoryPath),
+$array = path_to_array($directoryPath)
 file { $array[0]:
   ensure => 'directory',
 }
