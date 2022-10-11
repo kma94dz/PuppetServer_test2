1d2ee38175ede6d1754e5265ac1359ce25a31f7d
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 14:31:43 -0400

    Delete productionDebian directory

--- productionDebian/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-#include train::sequence
-#include train::appach_install
-#include train::mysql_install
-#include train::php_install
-file { '/tmp/fromGithub2.txt':
-  ensure => present,
-  content => path_to_array("/tmp/aa/bb/cc/dd/"),
-}
