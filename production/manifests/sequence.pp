656154776af12ae2567cc05533d14d4ee64c4442
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 13:00:50 -0400

    Update sequence.pp

--- production/manifests/sequence.pp
+++ production/manifests/sequence.pp
@@ -6,7 +6,7 @@
 #   include train::sequence
 class train::sequence {
 ########## on peut reecrir comme suit
-$directoryPath = '/tmp/a/b/c/dir4/'
+$directoryPath = '/tmp/a/b/c/dir4/',
 
 file { '/tmp/a/b/c/part1.txt':
   ensure => present,
@@ -18,7 +18,8 @@ file { ['/tmp/a/b/c/d/part2.txt', '/tmp/a/b/c/d/part3.txt']:
   ensure => present,
   require => File['/tmp/a/b/c/part1.txt'],
 }
-file { path_to_array($directoryPath):
+$array = path_to_array($directoryPath),
+file { $array:
   ensure => 'directory',
 }
 }
