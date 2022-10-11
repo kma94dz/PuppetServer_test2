601d62fe5f89e66d430ea08750d99efbd5c32fdd
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 11:35:08 -0400

    Add files via upload

---
+++ production/manifests/init.pp
@@ -0,0 +1,8 @@
+#include train::sequence
+#include train::appach_install
+#include train::mysql_install
+#include train::php_install
+file { '/tmp/fromGithub2.txt':
+  ensure => present,
+  content => path_to_array('from git hub'),
+}
