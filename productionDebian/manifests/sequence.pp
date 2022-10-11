a6452dd2db1c17161528f4e76b0886ade5ecc5d3
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 09:44:16 -0400

    Add files via upload

--- productionDebian/manifests/sequence.pp
+++ productionDebian/manifests/sequence.pp
@@ -10,11 +10,11 @@ $directoryPath = '/tmp/dir1/dir2/dir3/dir4/'
 
 file { '/tmp/dir1/dir2/dir3/part1.txt':
   ensure => present,
-  before => File[['/tmp/dir1/dir2/dir3/dir4/part2.txt', '/tmp/dir1/dir2/dir3/dir4/part3.txt']],
+  before => File[[$directoryPath+'part2.txt', $directoryPath+'part3.txt']],
   require => File[$directoryPath],
   content => String(path_to_array($directoryPath)),
 }
-file { ['/tmp/dir1/dir2/dir3/dir4/part2.txt', '/tmp/dir1/dir2/dir3/dir4/part3.txt']:
+file { [$directoryPath+'part2.txt', $directoryPath+'part3.txt']:
   ensure => present,
 }
 file { $directoryPath:
