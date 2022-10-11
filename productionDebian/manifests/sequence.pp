c200e575c290d537f78df43063f0c28cf63c9d63
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 09:31:01 -0400

    Add files via upload

---
+++ productionDebian/manifests/sequence.pp
@@ -0,0 +1,23 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include train::sequence
+class train::sequence {
+########## on peut reecrir comme suit
+$directoryPath = '/tmp/dir1/dir2/dir3/dir4/'
+
+file { '/tmp/dir1/dir2/dir3/part1.txt':
+  ensure => present,
+  before => File[[$directoryPath+'part2.txt', $directoryPath+'part3.txt']],
+  require => File[$directoryPath],
+  content => String(path_to_array($directoryPath)),
+}
+file { [$directoryPath+'part2.txt', $directoryPath+'part3.txt']:
+  ensure => present,
+}
+file { $directoryPath:
+  ensure => 'directory',
+}
+}
