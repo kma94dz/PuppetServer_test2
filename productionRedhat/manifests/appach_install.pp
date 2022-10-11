42a5f347016ceb47366464de575e54d2434623f8
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 08:22:33 -0400

    Update appach_install.pp

--- productionRedhat/manifests/appach_install.pp
+++ productionRedhat/manifests/appach_install.pp
@@ -5,8 +5,24 @@
 # @example
 #   include train::appach_install
 class train::appach_install {
-# execute 'dnf-get update'
-exec { 'dnf-update':                    # exec resource named 'dnf-update'
+# execute 'apt-get update'
+exec { 'dnf-update':                    # exec resource named 'apt-update'
   command => '/usr/bin/dnf update',  # command this resource will run
+  before => Exec['install-httpd'],
+}
+
+exec { 'install-httpd':                    # exec resource named 'apt-update'
+  command => '/usr/bin/dnf install httpd -y',  # command this resource will run
+}
+
+# install apache2 package
+package { 'httpd':
+  require => Exec['dnf-update'],        # require 'apt-update' before installing
+  ensure => installed,
+}
+
+# ensure apache2 service is running
+service { 'httpd':
+  ensure => running,
 }
 }
