10b477b18065511198be1c10fab351e1b1002f7e
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-11 12:30:10 -0400

    Update sequence.pp

--- production/manifests/sequence.pp
+++ production/manifests/sequence.pp
@@ -6,18 +6,18 @@
 #   include train::sequence
 class train::sequence {
 ########## on peut reecrir comme suit
-$directoryPath = '/tmp/dir1/dir2/dir3/dir4/'
+$directoryPath = '/tmp/a/b/c/dir4/'
 
-file { '/tmp/dir1/dir2/dir3/part1.txt':
+file { '/tmp/a/b/c/part1.txt':
   ensure => present,
-  before => File[['/tmp/dir1/dir2/dir3/dir4/part2.txt', '/tmp/dir1/dir2/dir3/dir4/part3.txt']],
-  require => File[$directoryPath],
+  before => File[['/tmp/a/b/c/d/part2.txt', '/tmp/a/b/c/d/part3.txt']],
+  require => File[path_to_array($directoryPath)],
   content => String(path_to_array($directoryPath)),
 }
-file { ['/tmp/dir1/dir2/dir3/dir4/part2.txt', '/tmp/dir1/dir2/dir3/dir4/part3.txt']:
+file { ['/tmp/a/b/c/d/part2.txt', '/tmp/a/b/c/d/part3.txt']:
   ensure => present,
 }
-file { $directoryPath:
+file { path_to_array($directoryPath):
   ensure => 'directory',
 }
 }
