7d0e7e11f7d3ce4ebc0ecdf91ae1b0b09d331319
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-12 13:19:41 -0400

    Update versioncmp.rb

--- production/lib/puppet/functions/versioncmp.rb
+++ production/lib/puppet/functions/versioncmp.rb
@@ -1,21 +1,36 @@
-Puppet::Functions.create_function(:'path_to_array') do
-  dispatch :pta do
-    param 'String', :path
-  end
-
-  def pta(path)
-    Array paths=[] 
-    dir=""
-    dir=dir + path[0]
-    for i in 1...path.length do
-      dir=dir + path[i]
-      if path[i]=='/' then
-        paths.push(dir)
+Puppet::Functions.create_function(:'versioncmp') do
+    dispatch :vcmp do
+      param 'String', :v1
+      param 'String', :v2
+    end
+  
+    def vcmp(v1,v2)  
+      version_array1 = v1.split(".")
+      version_array2 = v2.split(".")
+      result=0
+      for i in 0...[version_array1.length,version_array2.length].min do
+        if Integer(version_array1[i])>Integer(version_array2[i]) then
+            result = 2
+            break
+        end
+        elsif Integer(version_array1[i])==Integer(version_array2[i]) then
+            result = 1
+            break
+        end
+        else
+            result = 0
+            break
+        end
+    end
       end
+      if result == 1 then
+        if version_array1.length > version_array2.length  then
+            result = 2
+        elsif version_array1.length > version_array2.length then
+            result = 0
+        end
+      end
+      return result       
     end
-    if path[path.length-1] != '/' then
-      paths.push(dir)
-    end
-    return paths       
   end
-end
+  
