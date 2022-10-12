687d70661f02bd6c551e5f346348232cc39f2f4c
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-12 13:24:19 -0400

    Update versioncmp.rb

--- production/lib/puppet/functions/versioncmp.rb
+++ production/lib/puppet/functions/versioncmp.rb
@@ -1,35 +1,35 @@
 Puppet::Functions.create_function(:'versioncmp') do
     dispatch :vcmp do
-      param 'String', :v1
-      param 'String', :v2
+        param 'String', :v1
+        param 'String', :v2
     end
   
     def vcmp(v1,v2)  
-      version_array1 = v1.split(".")
-      version_array2 = v2.split(".")
-      result=0
-      for i in 0...[version_array1.length,version_array2.length].min do
-        if Integer(version_array1[i])>Integer(version_array2[i]) then
-            result = 2
-            break
+        version_array1 = v1.split(".")
+        version_array2 = v2.split(".")
+        result=0
+        for i in 0...[version_array1.length,version_array2.length].min do
+            if Integer(version_array1[i])>Integer(version_array2[i]) then
+                result = 2
+                break
+            end
+            elsif Integer(version_array1[i])==Integer(version_array2[i]) then
+                result = 1
+                break
+            end
+            else
+                result = 0
+                break
+            end
         end
-        elsif Integer(version_array1[i])==Integer(version_array2[i]) then
-            result = 1
-            break
+        if result == 1 then
+            if version_array1.length > version_array2.length  then
+                result = 2
+            elsif version_array1.length > version_array2.length then
+                result = 0
+            end
         end
-        else
-            result = 0
-            break
-        end
-      end
-      if result == 1 then
-        if version_array1.length > version_array2.length  then
-            result = 2
-        elsif version_array1.length > version_array2.length then
-            result = 0
-        end
-      end
-      return result       
+        return result       
     end
-  end
+end
   
