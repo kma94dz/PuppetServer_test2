9db62f1fb7ce599e7a41dfee927e82841721d5d5
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-12 11:48:26 -0400

    Update versioncmp.rb

--- production/lib/puppet/functions/versioncmp.rb
+++ production/lib/puppet/functions/versioncmp.rb
@@ -4,8 +4,8 @@ Puppet::Functions.create_function(:'versioncmp') do
       param  Integer, :v2
     end
   
-    def vcmp(v1,v2) 
-      return v1.to_i - v2       
+    def vcmp(v1,v2)
+      return v1.to_i - v2
     end
   end
   
