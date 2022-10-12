1353e7b3403a934e38127a96ac68427f0a82e6d6
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-12 08:41:05 -0400

    Delete production directory

--- production/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-include train::sequence
-#include train::appach_install
-#include train::mysql_install
-#include train::php_install
-file { '/tmp/fromGithub2.txt':
-  ensure => present,
-  content => String(path_to_array('a/b/c/d')),
-}
