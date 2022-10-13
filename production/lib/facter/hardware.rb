52cd3e0be43c7723f9cc09a41ac99dfc4b7bdfe5
Koalal Mohamed Amir <37029305+kma94dz@users.noreply.github.com>
2022-10-13 15:34:40 -0400

    Update hardware.rb

--- production/lib/facter/hardware.rb
+++ production/lib/facter/hardware.rb
@@ -3,14 +3,6 @@
 Facter.add(:hardware) do
   # https://puppet.com/docs/puppet/latest/fact_overview.html
   setcode do
-    distid = Facter.value('lsbdistid')
-    case distid
-    when /RedHatEnterprise|CentOS|Fedora/
-      'redhat'
-    when 'ubuntu'
-      'debian'
-    else
-      distid
-    end
+    'toto bobo'
   end
 end
